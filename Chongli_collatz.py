ad77bb073b29906c7b1e848c5741ef7681a3e66e
chongli <chongli.cn@gmail.com>
2015-09-11 23:22:10 -0400

    add comments

--- Chongli_collatz.py
+++ Chongli_collatz.py
@@ -1,5 +1,8 @@
 __author__ = 'cwang'
 
+# Collatz conjecture: x -> x * 3 + 1 or x -> x / 2 if x is odd or even.
+# Given B and k, determine how many numbers between 1 and B can go to 1 within k steps
+
 def collatz(B, k):
     steps = [None] * (B + 1)
     steps[1] = 0
@@ -21,6 +24,8 @@ def collatz(B, k):
             for i in range(k):
                 if path[i] <= B:
                     steps[path[i]] = -1
+                    # cannot reach in 2k steps, hence first k states are impossible
+                    # to reach in k steps
         else:
             number_of_steps = steps[path[-1]]
             for i in reversed(range(sz)):
@@ -29,7 +34,7 @@ def collatz(B, k):
                 if number_of_steps < k and number_of_steps >= 0:
                     number_of_steps += 1
                 elif number_of_steps == k:
-                    number_of_steps = -1
+                    number_of_steps = -1 # indicating impossibility within k steps
     # pairs = 0
     # for i, num in enumerate(steps):
     #     if i < B and steps[i] == steps[i + 1]:
@@ -37,12 +42,12 @@ def collatz(B, k):
     #     print(i, num)
 
     count = B - steps.count(-1)
-    print(steps.index(max(steps[1:])), max(steps[1:]))
-    # return count
+    # print(steps.index(max(steps[1:])), max(steps[1:]))
+    return count
 
 if __name__ == '__main__':
     B = 100000000
     k = 1000
     # print(collatz(B, k))
-    collatz(B, k)
+    print(collatz(B, k))
 
