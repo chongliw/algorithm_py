6df213234d86ea65b982bf8fa87342fe4240436d
hzj07 <h134679@126.com>
2015-09-11 23:09:23 -0400

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -1,36 +1,5 @@
 __author__ = 'cwang'
 
-def f(a):
-    if a % 2 == 0:
-        return a/2
-    else:
-        return 3*a + 1
-
-def count(b,k):
-    steps = [ -1 for i in range( b + 1) ]
-    steps[1] = 0
-    
-    for i in range(2,b):
-        if steps[i] == -1:
-            iterate_f_i = []
-            l = i
-            for j in range( 2 * k):
-                iterate_f_i.append(l)
-                l = f(l)
-                if  l < b and steps[l] != -1:
-                    break
-            len_iter = len(iterate_f_i)
-            if l < b and steps[l] != -1:
-                for j in range(len_iter):
-                    if iterate_f_i[len_iter - 1 - j] < b:
-                        steps[iterate_f_i[len_iter - 1 - j]] = steps[l] + j + 1
-            print steps
-            return steps
-                             
-print count(100,10)
-
-
-"""
 def collatz(B, k):
     steps = [None] * (B + 1)
     steps[1] = 0
@@ -76,4 +45,4 @@ if __name__ == '__main__':
     k = 1000
     # print(collatz(B, k))
     collatz(B, k)
-"""
+
