fc3c70a194b96dc4c11f70bf2070c593f0e7ac50
chongli <chongli.cn@gmail.com>
2015-09-20 21:16:15 -0400

    remove deplicated linked list https://leetcode.com/problems/remove-duplicates-from-sorted-list-ii/

---
+++ Chongli_remove_duplicated_linked_list.py
@@ -0,0 +1,34 @@
+__author__ = 'cwang'
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution(object):
+    def deleteDuplicates(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        currPos = head
+        inDistinct = True
+        init = False
+        dhead, distLN = None
+        while currPos is not None:
+            currVal = currPos.val
+            if currPos.next is None or currVal != currPos.next.val:
+                if inDistinct:
+                    if not init:
+                        distLN = ListNode(currVal)
+                        dhead = distLN
+                        init = True
+                    else:
+                        newLN = ListNode(currVal)
+                        distLN.next = newLN
+                        distLN = newLN
+                inDistinct = True
+            elif inDistinct:
+                inDistinct = False
+            currPos = currPos.next
+        return dhead
